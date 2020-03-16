30db70b2bf6a0cb736379f35bfea7526c1b7cbcd
MintyU <deathcat@outlook.kr>
2020-03-17 01:09:08 +0900

    #jwt authenticate

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
+server.express.use(passport.authenticate("jwt"));
 server.start({ port: PORT }, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
 );
